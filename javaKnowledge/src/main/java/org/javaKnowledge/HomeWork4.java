e38d6b243df0fe1aaf99ff269adb0f7b2a01d15a
jonmd87 <jonmd87@yandex.com>
2023-04-08 13:53:26 +0200

    homeWork4 started

---
+++ javaKnowledge/src/main/java/org/javaKnowledge/HomeWork4.java
@@ -0,0 +1,16 @@
+package org.javaKnowledge;
+
+import java.util.Scanner;
+
+public class HomeWork4 {
+    static private final String EXIT = "exit";
+    static private final String PRINT = "print";
+    static private final String REVERT = "revert";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (!EXIT.equals(input = scanner))
+    }
+
+}
